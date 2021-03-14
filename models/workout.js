f3d5890d7df89740917ad04c99d5a71b88513589
Said Mg <saidmghabghab@gmail.com>
2021-03-14 19:57:08 -0400

    setting up the models directory

---
+++ models/workout.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        unique: true
+    },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
