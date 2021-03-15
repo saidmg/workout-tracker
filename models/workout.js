5c80b3741d34228f05a045119b3fa8a1f5ed899b
Said Mg <saidmghabghab@gmail.com>
2021-03-14 20:13:43 -0400

    Completed the schema

--- models/workout.js
+++ models/workout.js
@@ -2,10 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        unique: true
-    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        distance: {
+            type: Number,
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }
+    ],
+    day: {
+        type: Date,
+        default: ()=> new Date()
+    }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
